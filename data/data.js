a7b1d95dc9be8863c8e6e2de66aee3e892d64ea6
Markus Mannheim <markus@mannheim.media>
2021-06-25 14:51:32 +1000

    Add files via upload

--- data/data.js
+++ data/data.js
@@ -2,42 +2,42 @@ const d3 = require("d3"),
       fs = require("fs");
 
 var areas = [
-  "Blue Mountains",
-  "Blacktown",
-  "Bayside",
-  "Hornsby",
-  "Georges River",
-  "Hawkesbury",
-  "Cumberland",
-  "Burwood",
-  "Hunters Hill",
-  "Canterbury-Bankstown",
-  "Central Coast",
-  "Wollongong",
-  "Shellharbour",
-  "Penrith",
-  "Parramatta",
-  "Canada Bay",
-  "Ku-ring-gai",
-  "Sutherland Shire",
-  "Camden",
-  "The Hills Shire",
-  "Inner West",
-  "Lane Cove",
-  "Campbelltown",
-  "Randwick",
-  "Northern Beaches",
-  "Fairfield",
-  "Strathfield",
-  "Mosman",
-  "Liverpool",
-  "Woollahra",
-  "Willoughby",
-  "Wollondilly",
-  "Waverley",
-  "Ryde",
-  "Sydney",
-  "North Sydney"
+  ["Blue Mountains", "sydney"],
+  ["Blacktown", "sydney"],
+  ["Bayside", "sydney"],
+  ["Hornsby", "sydney"],
+  ["Georges River", "sydney"],
+  ["Hawkesbury", "sydney"],
+  ["Cumberland", "sydney"],
+  ["Burwood", "sydney"],
+  ["Hunters Hill", "sydney"],
+  ["Canterbury-Bankstown", "sydney"],
+  ["Central Coast", "area"],
+  ["Wollongong", "area"],
+  ["Shellharbour", "area"],
+  ["Penrith", "sydney"],
+  ["Parramatta", "sydney"],
+  ["Canada Bay", "sydney"],
+  ["Ku-ring-gai", "sydney"],
+  ["Sutherland Shire", "sydney"],
+  ["Camden", "sydney"],
+  ["The Hills Shire", "sydney"],
+  ["Inner West", "sydney"],
+  ["Lane Cove", "sydney"],
+  ["Campbelltown", "sydney"],
+  ["Randwick", "sydney"],
+  ["Northern Beaches", "sydney"],
+  ["Fairfield", "sydney"],
+  ["Strathfield", "sydney"],
+  ["Mosman", "sydney"],
+  ["Liverpool", "sydney"],
+  ["Woollahra", "sydney"],
+  ["Willoughby", "sydney"],
+  ["Wollondilly", "sydney"],
+  ["Waverley", "sydney"],
+  ["Ryde", "sydney"],
+  ["Sydney", "sydney"],
+  ["North Sydney", "sydney"]
 ];
 
 var affected = [
@@ -54,7 +54,6 @@ fs.readFile("lgaBoundaries.geojson", "utf8", function(error, data) {
   var geoData = JSON
     .parse(data)
     .features
-
     .filter(function(d) {
       return d.properties.STE_CODE16 == "1";
     })
@@ -73,9 +72,9 @@ fs.readFile("lgaBoundaries.geojson", "utf8", function(error, data) {
   areas.forEach(function(d) {
     let match = geoData
       .filter(function(e) {
-        return e.properties.lga == d;
+        return e.properties.lga == d[0];
       });
-    match[0].properties.class = "watch";
+    match[0].properties.class = d[1];
     mapData.features.push(match[0]);
   });
 
@@ -84,7 +83,7 @@ fs.readFile("lgaBoundaries.geojson", "utf8", function(error, data) {
       .filter(function(e) {
         return e.properties.lga == d;
       });
-    match[0].properties.class = "stay";
+    match[0].properties.class = "affected";
     mapData.features.push(match[0]);
   });
 
